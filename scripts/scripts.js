207bf5baf494d3b053a5cd36fff223e2a007ba36
yaritxa-gonzalez <ya.gonzalezs@duocuc.cl>
2026-09-02 18:17:37 -0400

    agregar boton de perfil

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,44 @@ function inyectarFooter(){
 }
 
 function inyectarHeader(){
-    document.getElementById("header").innerHTML = "<section><h1 class='no-select'>The Seoul Food</h1></section><section><a href='#inicio'>inicio</a>";
+    document.getElementById("header").innerHTML = `
+        <section><h1 class='no-select'>The Seoul Food</h1></section>
+        <section><a href='#inicio'>inicio</a></section>
+        <div class="user-menu">
+            <button id="menu">
+                <box-icon type='solid' name='user' color="#fff"></box-icon>
+            </button>
+            <div id="userMenuDropdown" class="hidden">
+                <button id="inicio">Iniciar sesión</button>
+                <button id="registrase">Registrarse</button>
+            </div>
+        </div>
+    `;
+
+    activarMenuUsuario();
+}
+
+function activarMenuUsuario(){
+    var menu = document.getElementById("menu");
+    var userMenuDropdown = document.getElementById("userMenuDropdown");
+    var inicio = document.getElementById("inicio");
+    var registrase = document.getElementById("registrase");
+
+    menu.onclick = function () {
+        if (userMenuDropdown.classList.contains("hidden")) {
+            userMenuDropdown.classList.remove("hidden");
+        } else {
+            userMenuDropdown.classList.add("hidden");
+        }
+    };
+
+    inicio.onclick = function () {
+        window.location.href = "paginas/inicio_sesion.html";
+    };
+
+    registrase.onclick = function () {
+        window.location.href = "paginas/registrarse.html";
+    };
 }
 
 inyectarHeader();
