f4a85136e9e7f0653a8f0ac534bad38d638c8a24
jose-gonzalez106 <jo.gonzalez7@duocuc.cl>
2026-09-03 22:39:11 -0400

    generar tarjetas de producto dinámicamente desde productos.js

---
+++ scripts/pintarProductos.js
@@ -0,0 +1,23 @@
+function pintarProductos(){
+    var contenedor = document.querySelector(".contenedor");
+    if (!contenedor) return;
+
+    var html = "";
+
+    PRODUCTOS.forEach(function(producto){
+        html += `
+            <article class="caluga">
+                <a href="paginas/producto.html?id=${producto.id}" class="producto-link">
+                    <span class="nombre">${producto.nombre}</span>
+                    <img src="${producto.imagen}" alt="${producto.nombre}">
+                </a>
+                <span class="precio">Precio: $${producto.precio.toLocaleString("es-CL")}</span>
+                <button class="button">Agregar</button>
+            </article>
+        `;
+    });
+
+    contenedor.innerHTML = html;
+}
+
+pintarProductos();
